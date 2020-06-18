f101106a7317954647d244ba1d4c791829884aa3
emilme <emilme@sir.mle>
2020-06-18 12:48:52 -0700

    Form layout completed

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,7 +1,58 @@
 import React from 'react';
 
-function Form(){
-    return (<div>I am a placeholder test!</div>)
+function Form(props){
+    const {
+        values,
+        onSubmit,
+        onInputChange,
+        onCheckboxChange,
+    } = props
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <h2>This is a form!</h2>
+
+                <label>Name&nbsp;
+                    <input
+                        value={values.name}
+                        onChange={onInputChange}
+                        name='name'
+                        type='text'
+                    />
+                </label>
+
+                <label>Email&nbsp;
+                    <input
+                        value={values.email}
+                        onChange={onInputChange}
+                        name='email'
+                        type='text'
+                    />
+                </label>
+
+                <label>Password&nbsp;
+                    <input
+                        value={values.password}
+                        onChange={onInputChange}
+                        name='password'
+                        type='text'
+                    />
+                </label>
+
+                <label>Agree to Terms of Service
+                    <input
+                        name='terms of service'
+                        type="checkbox"
+                        onChange={onCheckboxChange}
+                        checked={values.terms}
+                    />
+                </label>
+
+                <button>Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default Form;
